57ca2546c96ca4f484890002deac1b4d8988b8c9
abi soniya <abisoniya1998@gmail.com>
2026-09-24 13:07:31 +0530

    update project

---
+++ src/component/app.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import Search from "./Search";
+import Product from "./Product";
+import About from "./About";
+import Footer from "./Footer";
+
+function App()
+{
+    return(
+        <div>
+            <Navbar></Navbar>
+        <Search></Search>
+        <Product></Product>
+        <About></About>
+        <Footer></Footer>
+        </div>
+    )
+}
+export default App;
